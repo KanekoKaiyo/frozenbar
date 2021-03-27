f5061857b23c11f6ec835122921cbae12551bbee
Alexandre PIRMEZ <alexandre.pirmez@condorcet.be>
2021-03-27 11:24:14 +0100

    Ajout rdm ddans monster

--- Monster.cs
+++ Monster.cs
@@ -6,7 +6,10 @@ namespace hackaton
 {
     class Monster : Entity
     {
-     
+        static Random rdm;
+        static Monster(){
+            rdm = new Random();
+        }
         public Monster(int x, int y)
             : base(x, y, "D:\\appez\\Download\\hackaton\\hackaton\\img\\invader1.gif")
         {
