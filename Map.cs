c3a18441f77b8a0a73feff7286e315b6530939d8
Alexandre PIRMEZ <alexandre.pirmez@condorcet.be>
2021-03-27 15:30:19 +0100

    fix missile

--- Map.cs
+++ Map.cs
@@ -131,7 +131,7 @@ namespace hackaton
                 {
                     MapGame[bul.X][bul.Y] = null;
                 }                
-                if (!(MapGame[bul.X][bul.Y-1] != null && MapGame[bul.X][bul.Y-1] is Wall))
+                if (bul.Y - 1 >= 0 && !(MapGame[bul.X][bul.Y-1] != null && MapGame[bul.X][bul.Y-1] is Wall))
                 {
                     MapGame[bul.X][bul.Y-1] = bul;
                     bul.Y--;
